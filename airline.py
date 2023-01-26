98d1dc849793561c500008a75d3eff4d001b947e
medherradi <med.herradi@gmail.com>
2023-01-26 16:12:18 -0100

    made some changes in the instance initializer

--- airline.py
+++ airline.py
@@ -1,7 +1,17 @@
 class Flight:
 
-   def __init__(self, number):
-      self._number = number
+    def __init__(self, number: str):
 
-   def get_flight_number(self):
-      return self._number
+        if not number[:2].isalpha():
+            raise ValueError(f"No airline code in '{number}'")
+
+        if not number[:2].isupper():
+            raise ValueError("the first 2 char must be uppercase")
+
+        if not (number[2:].isdigit() and int(number[2:]) < 1000):
+            raise ValueError("Invalid number please try later")
+
+        self._number = number
+
+    def get_flight_number(self):
+        return self._number
